06f19b361c04bd0925a65208f19c241ada70cdc1
Sethu Narayan Senthil <github@sethusenthil.com>
2024-03-21 00:06:08 -0400

    Make it more robust - Check if internet is available - Check multiple adnetworks - add custom adnetworks

--- lib/adblock_detector.dart
+++ lib/adblock_detector.dart
@@ -1,52 +1,118 @@
 library adblock_detector;
 
+import 'dart:io';
+
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 
+enum AdNetworks {
+  googleAdMob,
+  mediaNet,
+  appSumari,
+  startApp,
+  adColony,
+}
+
+/// A class that detects the presence of ad blockers.
 class AdBlockDetector {
-  // Check if adblock is enabled
-  Future<bool> isAdBlockEnabled() async {
-    // Define test URLs to check for adblock
-    Map<String, String> testURLs = {"Google AdMob": 'ad.doubleclick.net'};
+  /// Checks if AdGuard DNS is being used.
+  ///
+  /// Returns `true` if AdGuard DNS is being used, `false` otherwise.
+  Future<bool> isAdguardDNS() async {
+    if (await isInternetConnected()) {
+      bool adGuardDNS = false;
 
-    bool adBlockEnabled = false;
-
-    for (var entry in testURLs.entries) {
       try {
-        // Send a GET request to the test URL
-        Response res = await http.get(Uri.parse("https://${entry.value}"));
+        // Send a GET request to the AdGuard DNS check URL
+        Response res = await http.get(Uri.parse(
+            'https://g3-axkdouaxsfrp3ocfud-dnscheck.adguard-dns.com/dnscheck/test'));
 
-        // If the response status code is not 200, adblock is enabled
+        // If the response status code is not 200, AdGuard DNS is being used
         if (res.statusCode != 200) {
-          adBlockEnabled = true;
+          adGuardDNS = true;
         }
       } catch (e) {
-        // An exception occurred, adblock is enabled
-        adBlockEnabled = true;
-        break;
+        // An exception occurred, AdGuard DNS is NOT being used
       }
-    }
 
-    return adBlockEnabled;
+      return adGuardDNS;
+    }
+    return false;
   }
 
-  // Check if AdGuard DNS is being used
-  Future<bool> isAdguardDNS() async {
-    bool adGuardDNS = false;
-
+  /// Checks if the device is connected to the internet.
+  ///
+  /// Returns `true` if the device is connected to the internet, `false` otherwise.
+  Future<bool> isInternetConnected() async {
     try {
-      // Send a GET request to the AdGuard DNS check URL
-      Response res = await http.get(Uri.parse(
-          'https://g3-axkdouaxsfrp3ocfud-dnscheck.adguard-dns.com/dnscheck/test'));
-
-      // If the response status code is not 200, AdGuard DNS is being used
-      if (res.statusCode != 200) {
-        adGuardDNS = true;
+      final result = await InternetAddress.lookup(
+          'example.com'); //!! REMEMBER: google.com and other domains may not be resolvable in some places like China
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        return true;
       }
-    } catch (e) {
-      // An exception occurred, AdGuard DNS is NOT being used
+      return false;
+    } on SocketException catch (_) {
+      return false;
     }
+  }
 
-    return adGuardDNS;
+  /// Checks if ad blocking is enabled.
+  ///
+  /// The [testAdNetworks] parameter is a list of predefined ad networks to test.
+  /// The [customAdNetworks] parameter is an optional list of custom ad network URLs to test.
+  ///
+  /// Returns `true` if ad blocking is enabled, `false` otherwise.
+  Future<bool> isAdBlockEnabled(
+      {required List<AdNetworks> testAdNetworks,
+      List<Uri>? customAdNetworks}) async {
+    if (await isInternetConnected()) {
+      Map<AdNetworks, String> urlMap = {
+        AdNetworks.googleAdMob: 'ad.doubleclick.net',
+        AdNetworks.mediaNet: 'media.net',
+        AdNetworks.appSumari: 'api.appsamurai.com',
+        AdNetworks.startApp: 'api.fo.startappservice.com',
+        AdNetworks.adColony: 'clients-api.adcolony.com',
+      };
+
+      bool adBlockEnabled = false;
+
+      for (var testURL in testAdNetworks) {
+        try {
+          String url = urlMap[testURL]!;
+          // Send a GET request to the test URL
+          Response res = await http.get(Uri.parse("https://$url"));
+
+          // If the response status code is not 200, adblock is enabled
+          if (res.statusCode != 200) {
+            adBlockEnabled = true;
+          }
+        } catch (e) {
+          // An exception occurred, adblock is enabled
+          adBlockEnabled = true;
+          break;
+        }
+      }
+
+      if (customAdNetworks != null) {
+        for (var testURL in customAdNetworks) {
+          try {
+            // Send a GET request to the test URL
+            Response res = await http.get(testURL);
+
+            // If the response status code is not 200, adblock is enabled
+            if (res.statusCode != 200) {
+              adBlockEnabled = true;
+            }
+          } catch (e) {
+            // An exception occurred, adblock is enabled
+            adBlockEnabled = true;
+            break;
+          }
+        }
+      }
+
+      return adBlockEnabled;
+    }
+    return false;
   }
 }
